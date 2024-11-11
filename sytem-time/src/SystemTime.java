835e52734dedf18c0d9f7cc96932595154c45887
naut74 <ngoctuan14032001@gmail.com>
2024-11-11 11:13:49 +0700

    ffirst commit

---
+++ sytem-time/src/SystemTime.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
+
+// sout + Tab == System.out.println()
+// psvm + TAB == tạo nhanh hàm main().
